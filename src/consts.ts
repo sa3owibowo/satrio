957bfc6211173d07cd9e6f18ceca26e06f0fc742
Satrio Wibowo <me@satrio.net>
2026-02-28 04:00:40 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Satrio Wibowo",
+  DESCRIPTION: "I Am Nobody.",
+  AUTHOR: "Satrio Wibowo",
 }
 
 // Work Page
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "me@satrio.net",
+    HREF: "mailto:me@satrio.net",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "Satrio Wibowo",
+    HREF: "https://github.com/sa3owibowo"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "Satrio Wibowo",
+    HREF: "https://www.linkedin.com/in/sa3owibowo/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "Satrio Wibowo",
+    HREF: "https://twitter.com/sa3owibowo",
   },
 ]
 
