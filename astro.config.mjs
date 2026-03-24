3ed30eb5d33337fcdb89a688d52d8ed022045e6f
Satrio Wibowo <me@satrio.net>
2026-03-24 16:08:31 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
