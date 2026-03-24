380db83c89f75ce2bc5af05af3ddf4a2d378f657
Satrio Wibowo <me@satrio.net>
2026-03-24 16:22:28 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import Analytics from "@astrojs/analytics"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://satrio.net",
-  integrations: [webAnalytics(), mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
 
